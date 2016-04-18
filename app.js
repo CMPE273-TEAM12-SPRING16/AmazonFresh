59d94bc8d1de5249202abe56ba488672930f0f69
Gaurav Chodwadia <gauravchodwadia@gmail.com>
2016-04-17 22:20:22 -0700

    npm init + app.js

---
+++ app.js
@@ -0,0 +1,49 @@
+
+var express = require('express')
+  , http = require('http')
+  , path = require('path');
+var expressSession = require("express-session");
+var mongoStore = require("connect-mongo")(expressSession);
+var mongoSessionConnectURL = "mongodb://localhost:27017/amazonfreshdb";   //Change this if needed ................................//
+
+var app = express();
+app.use(expressSession({
+	secret: 'fjklowjafnkvnap',
+	resave: false,  //don't save session if unmodified
+	saveUninitialized: false,	// don't create session until something stored
+	duration: 30 * 60 * 1000,
+	activeDuration: 5 * 60 * 1000,
+	store: new mongoStore({
+		url: mongoSessionConnectURL
+	})
+}));
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+//All GET methods...........................//
+app.get('/signup', home.signup);                     // Change this..........................................//
+
+
+//All POST methods.........................//
+app.post('/signUpUser', users.signUpUser);           // Change this..........................................//
+
+
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('AmazonFresh Node-Server listening on port ' + app.get('port'));
+});
