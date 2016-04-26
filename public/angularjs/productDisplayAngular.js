3690f7af54d37759e9c590f7c78d9b134488c157
Ekta Sorathia <ektasorathia@gmail.com>
2016-04-25 20:49:38 -0700

    Hide-Show when no reviews

--- public/angularjs/productDisplayAngular.js
+++ public/angularjs/productDisplayAngular.js
@@ -4,6 +4,7 @@ productDisplayAngular.controller("ProductDisplayAngular",['$scope','$http','send
         $scope.reviewReq = true;
         $scope.cart = [];
         $scope.isLoggedIn = false;
+        $scope.noReview = true;
         $scope.checkOutBtnClass = "btn btn-primary btn-block btn-proceed-to-checkout-disabled";
 
             $http({
@@ -16,6 +17,9 @@ productDisplayAngular.controller("ProductDisplayAngular",['$scope','$http','send
 
             }).then(function (res) {
                 $scope.displayProductDetails = res.data.productDetails;
+               if(res.data.productDetails.REVIEW_DETAILS.length!=0){
+                    $scope.noReview = false;
+               }
             });
 
             $http({
