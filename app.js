6d976ef2665fce7895ccb65d40c3ce08c987f072
Gaurav Chodwadia <gauravchodwadia@gmail.com>
2016-04-26 23:22:35 -0700

    Error resolved in app.js

--- app.js
+++ app.js
@@ -63,6 +63,7 @@ app.get('/checkout', function(req, res){
 
 app.get('/map', function(req, res){
   res.render('trackOnMap', {});
+});
 
 app.get('/PastPurchases', function(req, res){
 	res.render('purchaseHistory', {});
@@ -203,7 +204,7 @@ app.get('/myProfile', function(req, res){
 
 app.get('/addProductTemplate', function(req, res) {
 	res.render('farmerTemplate/addProduct', {});
-})
+});
 app.get('/trackOrder/:billId',cart.doTrackOrder);
 app.post('/doTrackOrder',cart.getTrackOrder);
 app.post('/getProductDetails',product.getProductDetails);
